abe7ed45dcd2e5d4e7d88e2820604b3ab6bee51f
Linrui-ivy <zhao.linrui@ufl.edu>
2025-06-20 10:48:01 +0800

    Docs update v2 (#4)
    * Update training-related RST docs and figures for s2v method
    
    * Update title from Competition to Contest
    
    * Update competition content in evaluate.rst and index.rst
    
    * Update competition content and README
    
    * docs: 添加 html_title，更新页面顶部标题
    
    ---------
    
    Co-authored-by: Linrui-ivy <zhao.linrui@uf.edu>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'RLSolver Competition Documentation'
+project = 'RLSolver Contest Documentation'
 copyright = '2025, SecureFinAI Lab'
 author = 'SecureFinAI Lab'
 release = '1.0'
